1e6b39da05351992f63b3d8b967441de700fd0f7
Christopher George <cmgeorge@andrew.cmu.edu>
2020-03-25 12:10:16 -0400

    missing saving data only

--- binary_classifier/labeling.py
+++ binary_classifier/labeling.py
@@ -10,10 +10,12 @@ from PIL import ImageTk, Image
 ####################################
 # customize these functions
 ####################################
-
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+numbers = '0123456789'
 def init(data):
     data.file_path = './data/design_dz-cleaned.json'
     data.titlesSeen = set()
+    data.new_data = []
     data.example = None
     get_next_item(data)
 
@@ -25,14 +27,11 @@ def get_next_item(data):
         # if there aren't any images, or any text, or the title has already been seen,
         # ignore it
         if (len(example['images']) > 0 and len(example['text']) > 0 and
-           example['title'] not in data.titlesSeen):
-            print(example)
+           example['title'] not in data.titlesSeen and len(example['text']) < len(alphabet)
+           and len(example['images']) < len(numbers)):
             # List the text by parts on one side, list the images on the other.
             data.example = example
-            # for index, image_name in enumerate(data.example['images']):
-            #     name = 'design/' + image_name
-            #     print(name)
-                # data.example[index] = PhotoImage(file=name)
+            data.titlesSeen.add(example['title'])
 
             break
             # 1) Write to new seen list
@@ -44,9 +43,24 @@ def mousePressed(event, data):
     # use event.x and event.y
     pass
 
-def keyPressed(event, data):
-    # use event.char and event.keysym
+def save_item(data):
     pass
+
+def keyPressed(event, data):
+    # Remove mispress
+    if event.keysym in data.new_data:
+        data.new_data.remove(event.keysym)
+    # print(event.keysym)
+    elif event.keysym in numbers:
+        if int(event.keysym) < len(data.example['images']):
+            data.new_data.append(event.keysym)
+    elif event.keysym in alphabet:
+        if alphabet.index(event.keysym) < len(data.example['text']):
+            data.new_data.append(event.keysym)
+    elif event.keysym == 'space':
+        # TODO: Save Item
+        data.new_data = []
+        get_next_item(data)
 
 def timerFired(data):
     pass
@@ -60,13 +74,28 @@ def redrawAll(canvas, data):
         name = './design/' + image_name
         image = Image.open(name)
 
-        h_percent = (base_height/float(image.size[1]))
+        h_percent = (base_height/float(image.size[0]))
         wsize = int((float(image.size[0])*float(h_percent)))
         image = image.resize((base_height, wsize), Image.ANTIALIAS)
         image = ImageTk.PhotoImage(image)
 
         data.imgs.append(image)
-        canvas.create_image(540, index*base_height, anchor=NW, image=image)
+        canvas.create_image(720, index*base_height, anchor=NW, image=image)
+        if str(index) in data.new_data:
+            canvas.create_text(720 - 60, index*base_height, text='Active:'+str(index)+'.', font="Arial 14 bold", anchor=NW)
+        else:
+            canvas.create_text(720 - 20, index*base_height, text=str(index)+'.', font="Arial 14 bold", anchor=NW)
+
+
+    t = ''
+    for index, text in enumerate(data.example['text']):
+        if alphabet[index] in data.new_data:
+            t += 'Active: ' + alphabet[index] + '. ' + text + '\n\n'
+        else:
+            t += alphabet[index] + '. ' + text + '\n\n'
+    text_size = '14' if  index <= 10 else '12'
+    # print(len(t))
+    canvas.create_text(10, 10, text=t, font="Arial "+text_size+" bold", anchor=NW, width=data.width // 2)
 
 ####################################
 # use the run function as-is
