d4945e42b2f551ad41a3a42f7523c52e9f988586
Ubuntu <ubuntu@ip-172-31-43-109.ec2.internal>
2020-05-02 14:44:19 +0000

    added image folder google drive download

--- multilabel_classifier/run.sh
+++ multilabel_classifier/run.sh
@@ -1,5 +1,7 @@
 pip install gdown
 gdown https://drive.google.com/uc?id=1YXEokxwJ-97RXO_y9FU_hnC4Yaff-WIe
 tar -xvf dataset.tar
+gdown https://drive.google.com/uc?id=1AqIpFhuoV9_7TinEqDuhrwGj3gA8y9zm
+tar -xvf image_folder_data.tar
 pip3 install spacy
 python3 -m spacy download en_core_web_lg
